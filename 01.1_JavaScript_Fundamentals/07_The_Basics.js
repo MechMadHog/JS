fefc0e5d36be16c33fcc2a799443486751f6cfce
Michael MacDonagh <michaelgmacd@gmail.com>
2019-05-05 19:49:45 +0100

    Creating Empty Links and String Manipulations.

---
+++ 01.1_JavaScript_Fundamentals/07_The_Basics.js
@@ -0,0 +1,29 @@
+/* Empty Hyperlinks: */
+<a href="javascript: void(0);" />;
+// When CLICKED this wil ACTUALLY do nothing.
+
+/* String Manipulation: */
+// There are 3 types of Variables:
+// string (A variable that contains letters, symbols or spaces).
+var stringVar = "Example, String";
+// integer (Whole Numbers).
+var integerVar = 13;
+// float (A decimal or a Negative Number).
+var floatVar = 192.168;
+
+// The Length of a Variable:
+var theABCs = "abcdefghijklmnopqrstuvwxyz";
+document.write(theABCs.length);
+// Output will be: 26
+// variable.length can ONLY count characters INSIDE of a string (INSIDE of quotes).
+
+/* UpperCase & LowerCase: */
+var upperSample = "make me big";
+document.write(upperSample.toUpperCase());
+// Output will be: MAKE ME BIG
+
+var lowerSample = "MAKE ME SMALL";
+document.write(lowerSample.toLowerCase());
+// Output will be: make me small
+
+// Commonly used to compare input, like creating a case-sensitive password.
