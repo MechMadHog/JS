2ab7a3377620867dde8f0de7dd4fc3360e222388
Michael MacDonagh <michaelgmacd@gmail.com>
2019-05-05 23:54:45 +0100

    Add files via upload

---
+++ 01.1_JavaScript_Fundamentals/10_The_Basics.js
@@ -0,0 +1,18 @@
+// Switch Statements:
+//The code that is executed is ENTIRELY based on the answer that is provided.
+var name = "Michael";
+switch (name /* The switch variable */) {
+  case "Chris": // if
+    alert("Hello, Chris!");
+    break;
+  case "Ardan": // else if
+    alert("Hello, Ardan!");
+    break;
+  case "Michael": // else if
+    alert("Hello, Michael!"); //What I'm Actually Looking For.
+    break;
+  /* else */ default:
+    alert("The name Variable Doesn't Match Any Name Above? ");
+    break;
+}
+// Switch can use as many cases as necessary.
